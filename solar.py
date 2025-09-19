92511f7b7e697fc6ee8f9e917853a899e968c74f
MrHumanRebel <szekelydani5g@gmail.com>
2025-09-19 20:19:53 +0200

    used_quote

--- solar.py
+++ solar.py
@@ -256,6 +256,7 @@ def process_message(message_text, battery, power, state, current_condition, sunr
             f"RAM Usage: {ram}\n"
             f"CPU Usage: {cpu}\n"
             f"CPU Temp: {temps.get('cpu-thermal') or temps.get('CPU') or 'N/A'}\n"
+            f"Quote usage: {used_quote} / {QUOTE_LIMIT} ({(used_quote / QUOTE_LIMIT) * 100:.2f}%)"
         )
         send_telegram_message(message)
     if message_text == "/start":
